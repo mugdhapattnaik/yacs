9fb5503d5f592bfb63aa0a13ce33c7c4005cd6c4
Mugdha Pattnaik <mugdhapattnaik@gmail.com>
2020-11-29 23:01:59 +0530

    added master and worker

---
+++ master.py
@@ -0,0 +1,130 @@
+#!/usr/bin/python
+
+import json
+import socket
+import time
+import sys
+import random
+
+
+from queue import SimpleQueue
+
+class Master:
+
+    def __init__(self, config, sch_algo='RR'):
+        self.request_queue = SimpleQueue()
+        self.workers = config["workers"]
+        self.available_slots = {}
+        
+        if sch_algo == 'RR':
+            self.sch_algo = self.round_robin
+        elif sch_algo == 'RANDOM':
+            self.sch_algo = self.random
+        elif sch_algo == 'LL':
+            self.sch_algo = self.least_loaded
+        
+        for worker in self.workers:
+            self.available_slots[worker["worker_id"]] = worker["slots"]
+        #have to define shared variable(can be queue of requests) - techinically master doesn't care about slots, 
+        #only the scheduler does - can keep scheduler independent
+
+    def random(self):
+        free_slot_found = False
+        worker_ids = []
+        for w in self.workers:
+            worker_ids.append(w["worker_id"]) 
+        while not free_slot_found:
+            worker = worker_ids[random.randrange(0, (len(self.workers)))]
+            if self.available_slots[worker] > 0: 
+                free_slot_found = True
+                break
+        return worker
+
+    def round_robin(self):
+        free_slot_found = False
+        #they said it will be sortedd, idk if they meant that we assume it is sorted or we have to sort
+        #so I just sorted it 
+        for w in self.workers:
+            worker_ids.append(w["worker_id"]) 
+        worker_ids.sort()
+        
+        for worker in worker_ids:
+            if self.available_slots[worker] > 0:
+                free_slot_found = True
+        return worker    
+
+    def least_loaded(self):
+        free_slot_found = False
+        while not free_slot_found:
+            max_slots = max(self.available_slots.values())
+            if max_slots == 0:
+                time.sleep(1)
+            else:
+                #to get key from value
+                worker = list(self.available_slots.keys())[self.available_slots.values().index(max_slots)]
+                free_slot_found = True
+                break
+        return worker
+
+    def parse(request):
+        if len(request["map_tasks"]) == 0:
+            return False
+        else:
+            return True
+
+    def schedule(self, request):
+        map_tasks = request["map_tasks"]
+        for mapper in map_tasks:
+            w = self.sch_algo() #returns a worker that is free for task
+            self.available_slots[w] -= 1
+            t = (mapper["task_id"], w)
+            self.request_queue.put(t)
+            self.send_task(mapper, w)
+        
+        reduce_tasks = request["reduce_tasks"]
+        for reducer in reduce_tasks:
+            w = self.sch_algo() #returns a worker_id that is free for task
+            self.available_slots[w] -= 1
+            t = (reducer["task_id"], w)
+            self.request_queue.put(t)
+            self.send_task(reducer, w)
+
+    def send_task(self, task, worker):
+        host = 'localhost' #TBD
+        port = 1
+        for w in self.workers:
+            if w["worker_id"] == w:
+                port = int(w["port"])
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as c:
+            c.connect((host,port))
+            message = json.dumps(tasks).encode()
+            c.send(message)
+
+
+if __name__ == '__main__':     
+
+    requests_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    worker_updates_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    host = 'localhost'
+    requests_port = 5000
+    worker_updates_port = 5001
+
+    requests_socket.bind((host, requests_port))
+    requests_socket.listen(3)
+
+    #workers send updates to this port
+    worker_updates_socket.bind((host, worker_updates_port))
+    worker_updates_socket.listen(3)
+
+    
+    config_file = open(sys.argv[1], 'r')
+    config = json.load(config_file)
+    scheduling_algo = str(sys.argv[2])
+    master = Master(config, scheduling_algo)
+
+    while True:
+        conn, addr = requests_socket.accept()
+        request = conn.recv(2048).decode()
+        if master.parse(request):
+            master.schedule(request)
+    conn.close()
---
+++ worker.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+
+import json
+import socket
+import time
+import sys
+import random
+
+
+
+class Worker:
+
+    def __init__(self):
+        self.num_active_slots = 0
+        self.slots = 0
+
+    #incoming tasks must be queued and scheduled into slots(fcfs round robin cuz worker is dum)
+    def schedule(self):
+        pass
+
+    def send_updates(self):
+        pass
+
+    def run_task(self, task):
+        pass
+    
+
+
+if __name__ == '__main__':
+    host = 'localhost'
+    port = int(sys.argv[1])
+    worker_id = int(sys.argv[2])
+    
+    #have to get slots number from config file somehow - not sure if worker.py has access to config.json file(prolly doesn't)
+
+    task_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    task_socket.bind((host, port))    
+    task_socket.listen(5)
+
+    worker = Worker()
+    while True:
+        conn, addr = task_socket.accept()
+        task = conn.recv(2048).decode()
+        worker.run_task(task)
+    conn.close()
