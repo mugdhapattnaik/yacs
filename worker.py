381c265f3baf11bf688be969ade291d0a0b2e39b
ishi20 <iagarwal20@gmail.com>
2020-11-30 17:08:23 +0530

    Added scheduling and run_tasks

--- worker.py
+++ worker.py
@@ -16,22 +16,33 @@ class Worker:
         self.slots = list()
 
     #incoming tasks must be queued and scheduled into slots(fcfs round robin cuz worker is dum)
-    def schedule(self):
-        pass
+    def schedule(self,task):
+        task["start_time"] = time.time()
+        slot_lock.acquire()
+        if(self.num_active_slots < self.num_slots):
+            self.slots.append(task)
+            self.num_active_slots +=1
+        slot_lock.release()
 
     def send_updates(self):
         pass
 
-    def run_task(self, task):
-        pass
-    
+    def run_task(self):
+        while True:
+            for task in self.slots:
+                if(time.time() - task["start_time"] >= task["duration"]):
+                    slot_lock.acquire()
+                    self.slots.remove(task)
+                    self.num_active_slots -=1
+                    slot_lock.release()
+                    break
 
-
+slot_lock=threading.Lock()
 if __name__ == '__main__':
     host = 'localhost'
     port = int(sys.argv[1])
     worker_id = int(sys.argv[2])
-    
+
     #have to get slots number from config file somehow - not sure if worker.py has access to config.json file(prolly doesn't)
 
     task_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -39,9 +50,10 @@ if __name__ == '__main__':
     task_socket.listen(5)
 
     worker = Worker()
+    monitor_thread=threading.Thread(target=worker.run_task).start()
     while True:
         conn, addr = task_socket.accept()
         t = conn.recv(2048).decode()
         task = json.loads(t)
-        worker.run_task(task)
+        worker.schedule(task)
     conn.close()
